ce67ab89eb752d9a25b55aa3a74f11b7cd7e97c5
BuildTools <unconfigured@null.spigotmc.org>
2020-03-01 11:54:32 +0800

    complete api (GET)
    complete api (GET)

--- lib/api/jsong_parsing_map.dart
+++ lib/api/jsong_parsing_map.dart
@@ -19,17 +19,62 @@ Future<PostList> data;
   void initState() {
     super.initState();
     Network network = Network("https://jsonplaceholder.typicode.com/posts");
+    data = network.loadPosts(); 
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-    
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("PODO",)
+      ),
+      body: Center(
+        child:Container(
+          
+          child:FutureBuilder(
+            future: data,
+            builder: (context,AsyncSnapshot<PostList> snapshot){
+            List<Post> allPosts;
+            if(snapshot.hasData){
+              allPosts = snapshot.data.posts;
+
+              return createListView(allPosts, context);
+            }else{
+              return CircularProgressIndicator();
+            }
+          },)
+        )
+      ),
     );
   }
 }
 
-
+Widget createListView(List<Post> data,BuildContext context){
+  return Container(
+    child: ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (context,int index){
+        return Column(
+          children: <Widget>[
+            Divider(height: 5.0,),
+            ListTile(
+              title:(Text("${data[index].title}")),
+              subtitle: Text("${data[index].body}"),
+              leading: Column(
+                children: <Widget>[
+                  CircleAvatar(
+                  backgroundColor:Colors.amber,
+                  radius:23,
+                  child:Text("${data[index].id}")
+                )
+                ],
+              ),
+            ),
+          ],
+          );
+      }),
+  );
+}
 
 
 //fetch data from server
